3e72267544d28f91a0a3a7a5c9004d8b03880dfd
ganith2001 <55203230+ganith2001@users.noreply.github.com>
2025-01-18 20:50:56 +0530

    Create data.sql (#8)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO post (title, content) VALUES ('First Post', 'This is the content of the first post');
+INSERT INTO post (title, content) VALUES ('Second Post', 'Content of the second post');
